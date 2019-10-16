1381bf995d7637a7142d7504cdd45eec1558816e
Leopold Schabel <leo@nexantic.com>
2019-10-16 16:26:59 +0000

    Full command line

--- grafana.sh
+++ grafana.sh
@@ -4,8 +4,11 @@ docker run -d \
 	-p 80:3000 \
 	-v grafana-storage:/var/lib/grafana \
 	--name=grafana \
-	-e "GF_SERVER_ROOT_URL=https://ecsc.allesctf.net" \
-	-e "GF_SECURITY_ADMIN_PASSWORD=<snip>" \
+	-e GF_SERVER_ROOT_URL=https://ecsc.allesctf.net \
+	-e GF_SECURITY_ADMIN_PASSWORD=<snip> \
+	 -e GF_AUTH_ANONYMOUS_ENABLED=true \
+	 -e GF_AUTH_ANONYMOUS_ORG_NAME=Public \
+	 -e GF_AUTH_ANONYMOUS_ORG_ROLE=Viewer \
 	--restart=always \
-	grafana/grafana
+	grafana/grafana:5.4.5
 
